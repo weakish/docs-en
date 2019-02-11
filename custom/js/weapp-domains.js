2be6ff93cd83b776bd87df0bf7f52279356c12bd
Fuchen Shi <fuchens@uci.edu>
2019-02-11 08:51:08 -0800

    从中文文档同步样式

--- custom/js/weapp-domains.js
+++ custom/js/weapp-domains.js
@@ -6,6 +6,7 @@ angular.module('app').controller('WeappDomainsCtrl', [
     '$compile',
     function ($http, $scope, $rootScope, $timeout, $compile) {
         $scope.domains = {
+          upload: '正在获取配置',
           download: '正在获取配置'
         };
 
@@ -14,27 +15,19 @@ angular.module('app').controller('WeappDomainsCtrl', [
           console.log($rootScope.pageState.currentApp);
           if (currentApp) {
             // Magic: 通过 push_group 判断节点
-            if (currentApp.push_group === 'g0') {
-              var suffix = currentApp.app_id.slice(0, 8).toLowerCase();
-              $scope.extraRequestDomains = [
-                suffix + '.api.lncld.net',
-                suffix + '.engine.lncld.net',
-                suffix + '.rtm.lncld.net'
-              ];
-              $scope.requestDomainsLength = 6;
-            } else if (currentApp.push_group === 'q0'){
-              var suffix = currentApp.app_id.slice(0, 8).toLowerCase();
-              $scope.extraRequestDomains = [
-                'tab.leancloud.cn',
-                suffix + '.api.lncldapi.com',
-                suffix + '.engine.lncldapi.com',
-                suffix + '.rtm.lncldapi.com'
-              ];
-              $scope.requestDomainsLength = 7;
-            } else {
-              $scope.extraRequestDomains = [];
-              $scope.requestDomainsLength = 3;
+            var doamins = {
+              'g0': 'lncld.net',
+              'q0': 'lncldapi.com',
+              'a0': 'lncldglobal.com'
             }
+            var domain = doamins[currentApp.push_group];
+            var suffix = currentApp.app_id.slice(0, 8).toLowerCase();
+            $scope.extraRequestDomains = [
+              suffix + '.api.' + domain,
+              suffix + '.engine.' + domain,
+              suffix + '.rtm.' + domain
+            ];
+            $scope.requestDomainsLength = 4;
 
             AV.applicationId = undefined;
             AV.applicationKey = undefined;
@@ -43,19 +36,31 @@ angular.module('app').controller('WeappDomainsCtrl', [
               appKey: currentApp.app_key,
               masterKey: currentApp.master_key,
             });
-            new AV.File('weapp-domains-generator-test-file.txt', {
+            var file = new AV.File('weapp-domains-generator-test-file.txt', {
               base64: 'ZmVlbCBmcmVlIHRvIGRlbGV0ZSB0aGlzIGZpbGUu',
-            }).save().then(function(file) {
+            });
+            file._fileToken(undefined, {
+              useMasterKey: true
+            }).then(function(uploadInfo) {
+              console.log(uploadInfo);
               var downloadDomain;
-              var result = file.url().match(/\:\/\/([^\/]*)/);
+              var result = uploadInfo.url.match(/\:\/\/([^\/]*)/);
               if (result) {
-                downloadDomain = result[1];
+                downloadDomain = result[1].toLowerCase();
               } else {
-                throw new Error('invalid file url');
+                downloadDomain = '获取文件域名异常';
               }
-              console.log(downloadDomain);
               $scope.domains.download = downloadDomain;
+              var uploadDomain;
+              var result = uploadInfo.upload_url.match(/\:\/\/([^\/]*)/);
+              if (result) {
+                uploadDomain = result[1].toLowerCase();
+              } else {
+                uploadDomain = '获取文件域名异常';
+              }
+              $scope.domains.upload = uploadDomain;
               $scope.$digest();
+              file.id = uploadInfo.objectId;
               return file.destroy({
                 useMasterKey: true
               });
